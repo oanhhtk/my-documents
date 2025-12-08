b4fd15d8a1fea4ad0989c80bf1627d7bc5828eca
oanhhtk <oanhhuynh2402@gmail.com>
2025-12-08 22:32:25 +0700

    Add initial content and structure for GitLab CI/CD course and related pages

---
+++ constants/meta.ts
@@ -0,0 +1,18 @@
+export default {
+  index: "Introduction",
+  another: "Another Page",
+  advanced: "Advanced (A Folder)",
+  html: "HTML",
+  css: "CSS",
+  cicd: "Gitlab CI/CD",
+  about: {
+    title: "About",
+    type: "page",
+  },
+  contact: {
+    title: "Contact ↗",
+    type: "page",
+    href: "https://twitter.com/shuding_",
+    newWindow: true,
+  },
+};
